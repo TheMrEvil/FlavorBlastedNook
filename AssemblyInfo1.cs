e9d1336c59ee1c147511aba4d769a34aa06851f6
TheMrEvil <supersurplus99@gmail.com>
2025-07-19 05:39:32 -0500

    Add project files.

---
+++ AssemblyInfo1.cs
@@ -0,0 +1,5 @@
+using FlavorBlastedNook;
+using MelonLoader;
+
+[assembly: MelonInfo(typeof(Class1), "Flavor Blasted Nook", "1.0", "TheMrEvil")]
+[assembly: MelonGame("Alvios", "Vellum")]
---
+++ Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using MelonLoader;
+using HarmonyLib;
+using UnityEngine;
+
+namespace FlavorBlastedNook
+{
+    public class Class1 : MelonMod
+    {
+        public override void OnInitializeMelon()
+        {
+            LoggerInstance.Msg("Flavor Blasted Nook mod loaded!");
+        }
+
+        public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+        {
+            // Increase the nook limit when the scene loads
+            if (NookDB.DB != null)
+            {
+                NookDB.DB.NookLimit = 150;
+            }
+        }
+    }
+}
